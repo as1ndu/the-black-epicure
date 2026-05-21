56990cd02d1613c8545e33bf23c536e2e0b58f99
Asindu <4271996+as1ndu@users.noreply.github.com>
2026-05-21 23:15:33 +0300

    feat: Added Giscus comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,22 +1,38 @@
 'use client'
-
-import { Comments as CommentsComponent } from 'pliny/comments'
-import { useState } from 'react'
-import siteMetadata from '@/data/siteMetadata'
+import Giscus from '@giscus/react'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
-
-  if (!siteMetadata.comments?.provider) {
-    return null
-  }
+  // const giscus_script = `<script src="https://giscus.app/client.js"
+  //       data-repo="as1ndu/the-black-epicure"
+  //       data-repo-id="R_kgDOSjYTvw"
+  //       data-category="Ideas"
+  //       data-category-id="DIC_kwDOSjYTv84C9kBF"
+  //       data-mapping="pathname"
+  //       data-strict="0"
+  //       data-reactions-enabled="1"
+  //       data-emit-metadata="1"
+  //       data-input-position="top"
+  //       data-theme="catppuccin_frappe"
+  //       data-lang="en"
+  //       data-loading="lazy"
+  //       crossOrigin="anonymous"
+  //       async>
+  //     </script>`
   return (
-    <>
-      {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) : (
-        <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
-    </>
+    // <div dangerouslySetInnerHTML={{ __html: giscus_script }} />
+    <Giscus
+      repo="as1ndu/the-black-epicure"
+      repoId="R_kgDOSjYTvw"
+      category="Ideas"
+      categoryId="DIC_kwDOSjYTv84C9kBF"
+      mapping="pathname"
+      strict="0"
+      reactions-enabled="1"
+      emitMetadata="1"
+      input-position="top"
+      theme="catppuccin_frappe"
+      lang="en"
+      loading="lazy"
+    />
   )
 }
